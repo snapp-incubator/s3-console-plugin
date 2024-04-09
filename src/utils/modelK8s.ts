57495cef0ffb63fcf21f067026bec4bdcf723b31
meysam <meysam.jeffrey@gmail.com>
2024-04-09 13:48:05 +0330

    fix: change url routes

--- src/utils/modelK8s.ts
+++ src/utils/modelK8s.ts
@@ -14,7 +14,7 @@ export const mockSecrets = {
 };
 
 export const userClaimUrl =
-  'clusterserviceversions/s3-operator.v0.3.3/s3.snappcloud.io~v1alpha1~S3UserClaim';
+  'clusterserviceversions/s3-operator.v0.3.4/s3.snappcloud.io~v1alpha1~S3UserClaim';
 
 export const bucketUrl =
-  'clusterserviceversions/s3-operator.v0.3.3/s3.snappcloud.io~v1alpha1~S3Bucket';
+  'clusterserviceversions/s3-operator.v0.3.4/s3.snappcloud.io~v1alpha1~S3Bucket';
